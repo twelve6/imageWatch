c88e73e0cbb665100861e8b03cd4fb401e95044b
Luis <luis@twelve6.com>
2015-07-31 15:03:27 -0500

    dealy function needs to be integrated better.

--- imgWatch.js
+++ imgWatch.js
@@ -1,3 +1,11 @@
+var delay = (function(){
+  var timer = 0;
+  return function(callback, ms){
+    clearTimeout (timer);
+    timer = setTimeout(callback, ms);
+  };
+})();
+
 ;(function($) {
 
     var PluginName = 'imageWatch',
